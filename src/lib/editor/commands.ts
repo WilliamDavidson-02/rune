a9c76d9103caec796b2e3ab2ab36a32c81e070f6
William Davidson <wj.davidson@outlook.com>
2025-08-12 12:40:36 +0200

    feat: task completion toggle keymap function

--- src/lib/editor/commands.ts
+++ src/lib/editor/commands.ts
@@ -8,10 +8,11 @@ import {
 } from "@codemirror/commands"
 import { markdownKeymap } from "@codemirror/lang-markdown"
 import { searchKeymap } from "@codemirror/search"
-import { EditorSelection } from "@codemirror/state"
+import { EditorSelection, type ChangeSpec } from "@codemirror/state"
 import { EditorView, type KeyBinding } from "@codemirror/view"
 
 import { toggleBolde, toggleItalic } from "./emphasis"
+import { isCheckbox, toggleTask } from "./task"
 
 // Codemirror only has a method to insert a blank line under the cirrent line(s)
 export const insertBlankLineAbove = (view: EditorView) => {
@@ -20,6 +21,7 @@ export const insertBlankLineAbove = (view: EditorView) => {
 	const { changes, selection } = state.changeByRange((range) => {
 		const line = state.doc.lineAt(range.head)
 		const { from } = state.doc.line(Math.max(1, line.number - 1))
+
 		return {
 			range: EditorSelection.cursor(from),
 			changes: []
@@ -29,6 +31,38 @@ export const insertBlankLineAbove = (view: EditorView) => {
 	view.dispatch({ changes, selection })
 
 	return insertBlankLine(view)
+}
+
+export const handleInsertBlankLine = (view: EditorView) => {
+	const { state } = view
+
+	const { changes, selection } = state.changeByRange((range) => {
+		const changes: ChangeSpec[] = []
+		let line = state.doc.lineAt(range.from)
+
+		while (line.from <= range.to) {
+			if (isCheckbox(line.text)) {
+				changes.push(toggleTask(line))
+				line = state.doc.line(line.number + 1)
+				continue
+			}
+
+			break
+		}
+
+		return {
+			range,
+			changes
+		}
+	})
+
+	view.dispatch({ changes, selection })
+
+	if (changes.empty) {
+		return insertBlankLine(view)
+	}
+
+	return true
 }
 
 export const customKeymap: KeyBinding[] = [
@@ -52,6 +86,10 @@ export const reMappedKeymap = cmDefaultKeymap.map((keymap: KeyBinding) => {
 	// Replace default keymap Mod-i, so custom Mod-i can be used
 	if (keymap.run === selectParentSyntax) {
 		keymap.key = "Shift-Mod-i"
+	}
+
+	if (keymap.run === insertBlankLine) {
+		keymap.run = handleInsertBlankLine
 	}
 
 	return keymap
--- src/lib/editor/task.ts
+++ src/lib/editor/task.ts
@@ -1,13 +1,29 @@
+import type { Line } from "@codemirror/state"
 import { EditorView } from "@codemirror/view"
 
 import { type SyntaxNodeRef } from "./viewPlugin"
 
 const isTaskChecked = (line: string) => {
-	return /^\[x\]/i.test(line)
+	return /\[x\]/i.test(line)
+}
+
+export const isCheckbox = (line: string) => {
+	return /^\s*(- \[(x| )\]) /i.test(line)
 }
 
 export const getNodeTaskStatus = (node: SyntaxNodeRef, view: EditorView) => {
 	const { from, to } = node
 	const text = view.state.doc.sliceString(from, to)
 	return String(isTaskChecked(text))
+}
+
+export const toggleTask = (line: Line) => {
+	const startIndex = line.text.indexOf("- [")
+	const checkbox = isTaskChecked(line.text) ? "- [ ]" : "- [x]"
+
+	return {
+		from: line.from + startIndex,
+		to: line.from + startIndex + checkbox.length,
+		insert: checkbox
+	}
 }
